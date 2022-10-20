f128b92834b20f3f5e7e1ac46584a0c1d5adfa88
Shiv C Kushawah <70078746+shivck13@users.noreply.github.com>
2022-10-20 20:09:18 +0000

    PORT from environment variable for deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const YouTubeDownloader = require("./youtube-downloader.js")
 
 // 👉 initialisation
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // 👉 settings
 app.set("views", path.join(__dirname, "views"))
